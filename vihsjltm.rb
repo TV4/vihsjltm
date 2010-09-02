985ec4cd41af45ea7664ad580e26314152ef560c
brianjriddle <brianjriddle@gmail.com>
2010-09-02 12:28:46 +0200

    fixed i dont like that link.

--- vihsjltm.rb
+++ vihsjltm.rb
@@ -8,12 +8,14 @@ VEGAN = YAML::load(open('data2.yml'))
 
 get '/' do
   @i_dont_play_that ="JAG ÄTER FÖR I HELVETE INTE KÖTT"
+  @hit_me_again_link = "/"
   @i_dont_play_that_link="/vegan"
   @recipe_title, @recipe_link = find_dinner INGREDIENTS
   erb :index
 end
 
 get '/vegan' do
+  @hit_me_again_link = "/vegan"
   @i_dont_play_that="JAG ÄR VÄL FÖR I HELVETE INGEN GRÄSÄTARE"
   @i_dont_play_that_link="/"
   @recipe_title, @recipe_link = find_dinner VEGAN
